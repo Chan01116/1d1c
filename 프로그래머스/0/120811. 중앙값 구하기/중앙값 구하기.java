482a5971ee81e2bacc0edbd76909f48dfbba8763
Chan01116 <179061389+Chan01116@users.noreply.github.com>
2025-06-05 18:15:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.17 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] array) {
+        for(int i = 0; i < array.length - 1; i++){
+            int minIdx = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[j] < array[minIdx]){
+                    minIdx = j;
+                }
+                
+            }
+            int temp = array[i];
+            array[i] = array[minIdx];
+            array[minIdx] = temp;
+        }
+        return array[array.length / 2];
+    }
+}
